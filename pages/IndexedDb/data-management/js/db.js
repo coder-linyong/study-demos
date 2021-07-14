df4a73a5689f350bbc8d454c9dd35e3001f026f1
xionglinyong <1243010754@qq.com>
2021-07-14 17:41:06 +0800

    玩成基于IndexedDB的数据管理

--- pages/IndexedDb/data-management/js/db.js
+++ pages/IndexedDb/data-management/js/db.js
@@ -1,16 +1,18 @@
+/*
+* 注意：
+*   1.当代码从数据库操作请求到事件循环中时，事务会被关闭，也就是说所有请求必须同步发送，所以每次执行数据库操作时新开一个事物
+* */
 export default class Db {
   static storeName = 'commodity'
   static indexArr = ['type', 'brand']
-  static types=['家电','食品酒饮','家居厨具','手机','个护清洁','医药保健','数码','图书文娱',
-    '男装','电脑办公','运动户外','鞋靴','母婴童装','家居家装','内衣配饰','女装','宠物鲜花',
-    '生鲜','箱包','钟表珠宝','玩具乐器','气摩生活','美妆护肤','奢侈品','计生情趣','生活旅行',
-    '教育培训','工业品',]
+  static types = ['家电', '食品酒饮', '家居厨具', '手机', '个护清洁', '医药保健', '数码', '图书文娱',
+    '男装', '电脑办公', '运动户外', '鞋靴', '母婴童装', '家居家装', '内衣配饰', '女装', '宠物鲜花',
+    '生鲜', '箱包', '钟表珠宝', '玩具乐器', '气摩生活', '美妆护肤', '奢侈品', '计生情趣', '生活旅行',
+    '教育培训', '工业品',]
 
   dbName
   version
   db
-  transaction
-  store
 
   constructor(dbName, version) {
     this.dbName = dbName
@@ -45,12 +47,14 @@ export default class Db {
           db.close()
         }
 
-        const transaction = this.transaction = db.transaction(Db.storeName, 'readwrite')
-        this.store = transaction.objectStore(Db.storeName)
-
         resolve(ev.target.result)
       }
     })
+  }
+
+  getStore() {
+    const transaction = this.db.transaction(Db.storeName, 'readwrite')
+    return transaction.objectStore(Db.storeName)
   }
 
   put(data) {
@@ -61,7 +65,7 @@ export default class Db {
       }
     })
     return new Promise((resolve, reject) => {
-      const { store } = this
+      const store = this.getStore()
       const request = store.put(data)
       request.onsuccess = ev => {
         resolve(true)
@@ -78,12 +82,13 @@ export default class Db {
     }
 
     return new Promise((resolve, reject) => {
-      const { store } = this
+      const store = this.getStore()
       const request = store.delete(id)
       request.onsuccess = ev => {
         resolve(true)
       }
       request.onerror = ev => {
+        console.log(ev)
         reject(ev.target.error)
       }
     })
@@ -95,7 +100,7 @@ export default class Db {
     }
 
     return new Promise((resolve, reject) => {
-      const { store } = this
+      const store = this.getStore()
       const request = store.get(id)
       request.onsuccess = ev => {
         resolve(ev.target.result)
@@ -108,22 +113,23 @@ export default class Db {
 
   query(query) {
     return new Promise((resolve, reject) => {
-      const { store } = this
+      const store = this.getStore()
       let request
-      if (!query) {
+      if (!query || !(query.type || query.brand)) {
         request = store.getAll()
       } else {
         for (const name of Db.indexArr) {
-          if (name in query) {
+          if (name in query && query[name]) {
             request = store.index(name)
-              .getAll()
+              .getAll(query[name])
             break
           }
         }
       }
+
       if (request) {
         request.onsuccess = ev => {
-          resolve(ev.target.result)
+          resolve(ev.target.result || [])
         }
         request.onerror = ev => {
           reject(ev.target.error)
@@ -136,11 +142,13 @@ export default class Db {
 
   getEnum(name = 'type') {
     return new Promise((resolve, reject) => {
-      const { store } = this
+      const store = this.getStore()
       const request = store.index(name)
         .getAll()
       request.onsuccess = ev => {
-        resolve(ev.target.result)
+        const set=new Set()
+        const names=ev.target.result.map(item=>set.add(item[name]))
+        resolve(Array.from(set))
       }
       request.onerror = ev => {
         reject(ev.target.error)
