f9d6da65108a655c6381505b27de41f5cfa24d80
xionglinyong <xionglinyong@skieer.com>
2022-04-19 18:25:02 +0800

    更新错误捕获器

--- ts/catch-wrapper.ts
+++ ts/catch-wrapper.ts
@@ -1,10 +1,37 @@
 /**
- * 一个错误包裹器，包裹中的函数全部会在包裹器内被捕获
+ * 一个错误包裹器（无this版本），包裹中的函数全部会在包裹器内被捕获
  */
-type CatchWrappedFunction<Args extends any[], F extends (...args: Args) => any> = (this: ThisParameterType<F>, ...args: Args & Parameters<F>) => ReturnType<F>
 
-export function catchWrapper<Args extends any[], F extends (...args: Args) => any> (fun: F, catchHandler?: (e: Error) => void): CatchWrappedFunction<Args, F> {
-  return function (this: ThisParameterType<F>, ...args: Args) {
+/**
+ * 函数的错误包裹器，如果发生错误，会在包裹器内部捕获；可指定捕获处理函数
+ * @param fun 需要捕获的函数
+ * @param catchHandler 捕获处理函数
+ */
+const catchWrapper = <A extends Array<any>, R> (fun: (...args: A) => R, catchHandler?: (e: Error) => void) => {
+  return function (...args: A): R {
+    try {
+      return fun(...args)
+    } catch (e) {
+      if (catchHandler) {
+        catchHandler(e)
+      } else {
+        console.log('❗[user action collector]: an error occurred！！！')
+        console.error(e)
+      }
+    }
+  }
+}
+
+export type AnyFunction = (...args: any[]) => any;
+
+/**
+ * 函数的错误包裹器（this版本），如果发生错误，会在包裹器内部捕获；可指定捕获处理函数
+ * @param {F} fun
+ * @param {(e: Error) => void} catchHandler需要捕获的函数
+ * @returns {(...args: Parameters<F>) => ReturnType<F>}捕获处理函数
+ */
+function catchWrapperHasThis<F extends AnyFunction> (fun: F, catchHandler?: (e: Error) => void): ((...args: Parameters<F>) => ReturnType<F>) {
+  return function (this: ThisParameterType<F>, ...args: Parameters<F>) {
     try {
       return fun.apply(this, args)
     } catch (e) {
