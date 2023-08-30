5d69b854d3efb1ff2545512d3e44f19380dc7182
Xiong <1243010754@qq.com>
2023-08-30 22:48:27 +0800

    Update apply.js

--- js/apply.js
+++ js/apply.js
@@ -1,15 +1,16 @@
 function apply(fn, target, args) {
+  // 不是函数抛出错误
+  if (typeof fun !== 'function') {
+    throw Error('func not a function')
+  }
   // 传入值不对则为空数组
-  if (args === undefined || args === null) {
+  if (!Array.isArray(args)) {
     args = []
   }
   // 如果没有传入this，则使用全局this
-  if (target === null || target === undefined) {
-    target = window || global
+  if (target===null || !['function','object'].includes(typeof target)) {
+    context=globalThis
   }
-
-  // 包装成对象，方便设置属性
-  target = Object(target)
 
   // 当前函数设置到目标对象上，目标该函数运行的this就会是目标对象
   const targetFnKey = Symbol('targetFnKey')// Symbol防止属性名重复
